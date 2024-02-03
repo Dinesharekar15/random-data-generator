67b5e4a860a42c899126622cab6bdfd380d1a7cd
dinesh <arekardinesh685@gmail.com>
2024-02-04 00:30:09 +0530

    delete existing data also

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const cors = require('cors');
+
 const PORT=7000;
 
 const app=express();
 app.use(cors());
-app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/company');
 
-const employeeSchema=new mongoose.Schema({
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost:27017/company' );
+
+const employeeSchema=new mongoose.Schema({ 
     name:String,
     age:Number,
     salary:Number,
@@ -20,14 +22,16 @@ const Employee=mongoose.model('Employee', employeeSchema);
 
 app.post('/', async(req,res)=>{
     try{
+
+        await Employee.deleteMany({});
         const randomEmployee={
             name:`Employee${Math.floor(Math.random() * 1000)}`,
             age:Math.floor(Math.random()*50)+20,
             salary: Math.floor(Math.random() * 100000),
             position:`developer`
         }
-        const newEmployee=new Employee(randomEmployee);
-        await newEmployee.save();
+        await Employee.create(randomEmployee);
+        
         console.log('Employee saved successfully');
         res.status(200).json({ message: 'Random data generated and saved successfully!' });
 
